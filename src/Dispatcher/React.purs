44b96b387b5d5f27ae8c9002067c7a78af4f19ae
Jolse Maginnis <doolse@gmail.com>
2017-02-06 13:38:57 +1100

    Unsafe with ref

--- src/Dispatcher/React.purs
+++ src/Dispatcher/React.purs
@@ -4,6 +4,7 @@ module Dispatcher.React (
 , getRefs
 , modifyState
 , unsafeWithRef
+, unsafeWithRef'
 , emptyHandler
 , execHandler
 , createComponent
@@ -33,6 +34,7 @@ import Control.Monad.RWS (RWS, ask, execRWS, modify)
 import Control.Monad.Reader (ReaderT(ReaderT))
 import Control.Monad.Trans.Class (lift)
 import Data.Function.Uncurried (Fn4, runFn4)
+import Data.Maybe (Maybe(..))
 import Data.Tuple (fst)
 import Dispatcher (class Dispatchable, class FromContext, dispatch, fromContext)
 import React (GetInitialState, ReactClass, ReactElement, ReactRefs, ReactSpec, ReactThis, Read, ReadOnly, ReadWrite, Refs, Render, createClass, getChildren, readState, spec', transformState)
@@ -53,7 +55,7 @@ getProps :: forall m props state eff. MonadEff ( props :: R.ReactProps | eff ) m
 getProps =  reactReaderT (liftEff <<< R.getProps)
 
 -- | Get the React state
-getState :: forall m props state eff. MonadEff ( state :: R.ReactState ReadWrite | eff ) m => ReactReaderT props state m state
+getState :: forall m props state access eff. MonadEff ( state :: R.ReactState (read::Read|access) | eff ) m => ReactReaderT props state m state
 getState = reactReaderT (liftEff <<< readState)
 
 -- | Modify the React state
@@ -71,9 +73,15 @@ unsafeWithRef :: forall props state ref eff m. (MonadEff (refs::ReactRefs ReadOn
   (ref -> Eff (refs::ReactRefs ReadOnly|eff) Unit)
   -> String
   -> ReactReaderT props state m Unit
-unsafeWithRef f s = do
+unsafeWithRef f = void <<< unsafeWithRef' f
+
+unsafeWithRef' :: forall props state ref eff m a. (MonadEff (refs::ReactRefs ReadOnly|eff) m) =>
+  (ref -> Eff (refs::ReactRefs ReadOnly|eff) a)
+  -> String
+  -> ReactReaderT props state m (Maybe a)
+unsafeWithRef' f s = do
   refs <- getRefs
-  liftEff $ runFn4 mapRef f (pure unit) refs s
+  liftEff $ runFn4 mapRef (f >>> map Just) (pure Nothing) refs s
 
 -- | an empty event handler
 emptyHandler :: forall a m. (Applicative m) => a -> m Unit
@@ -90,7 +98,7 @@ class ReactSpecCreator eval initialstate render props state eff | eval initialst
 class ReactInitialState eval initialstate props state eff | initialstate -> state where
   createInitialState :: eval -> initialstate -> GetInitialState props state eff
 
-class ReactRender eval renderer props state eff where
+class ReactRender eval renderer props state eff | renderer -> eff where
   createRenderer :: eval -> renderer -> Render props state eff
 
 -- | Create a ReactClass with the given state, renderer and action evaluator
@@ -166,7 +174,7 @@ newtype ReactProps p = ReactProps p
 -- | Useful in renderers
 newtype ReactChildren = ReactChildren (Array ReactElement)
 
-instance stateFromContext :: TypeEquals state state2 => FromContext eval (ReactThis props state) (ReactState state2) Eff (state::R.ReactState (read::Read)|eff) where
+instance stateFromContext :: TypeEquals state state2 => FromContext eval (ReactThis props state) (ReactState state2) Eff (state::R.ReactState (read::Read|access)|eff) where
   fromContext _ = map (ReactState <<< to) <<< readState
 
 instance propsFromContext :: TypeEquals props props2 => FromContext eval (ReactThis props state) (ReactProps props2) Eff (props::R.ReactProps|eff) where

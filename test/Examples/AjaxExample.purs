b920c454102e6f77881ccfac26731b778c294f1a
Don <don.abrams@smartrac-group.com>
2017-04-12 17:23:15 -0400

    tests fixed, yay! Too bad generic deriving doesn't work like it used to

--- test/Examples/AjaxExample.purs
+++ test/Examples/AjaxExample.purs
@@ -1,11 +1,14 @@
 module Examples.Ajax where
 
 import Prelude
+import Control.Apply (lift2)
 import Control.Monad.Except (runExcept)
 import Control.Monad.Trans.Class (lift)
 import Data.Either (either)
-import Data.Foreign.Generic (genericDecodeJSON, defaultOptions)
-import Data.Generic.Rep (class Generic)
+import Data.Foreign (readString)
+import Data.Foreign.Class (class Decode)
+import Data.Foreign.Index (readProp)
+import Data.Foreign.Generic (decodeJSON)
 import Data.Maybe (Maybe(..), maybe)
 import Dispatcher.React (createLifecycleComponent, didMount, getProps, modifyState)
 import Global (encodeURIComponent)
@@ -17,7 +20,11 @@ import React.DOM.Props (src)
 data Action = FetchMovie
 
 newtype Movie = Movie { title :: String, poster :: String}
-derive instance genericMovie :: Generic Movie _
+instance decodeMovie :: Decode Movie where
+  decode f = do
+    titleT <- readString <$> readProp "Title" f
+    posterT <- readString <$> readProp "Poster" f
+    lift2 ( \title poster -> Movie { title, poster } ) titleT posterT
 
 type State = {movie::Maybe Movie}
 
@@ -26,11 +33,11 @@ ajax = createFactory $ createLifecycleComponent (didMount FetchMovie) {movie:Not
   where
     render {movie} = div' $ maybe nomovie showMovie movie
       where
-      showMovie (Movie {title,poster}) = [ h1' [ text title ], img [src poster] [] ]
+      showMovie (Movie {title, poster}) = [ h1' [ text title ], img [src poster] [] ]
       nomovie = [ h1' [ text "Waiting for movie response" ] ]
 
     eval FetchMovie = do
       {title} <- getProps
       s <- lift $ get $ "http://www.omdbapi.com/?t=" <> encodeURIComponent title <> "&y=&plot=short&r=json"
-      let movie = either (const $ Nothing) Just $ runExcept $ genericDecodeJSON defaultOptions s.response
+      let movie = either (const $ Nothing) Just $ runExcept $ decodeJSON s.response
       modifyState _ {movie=movie}
